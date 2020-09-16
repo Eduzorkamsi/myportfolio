731e792725c029525b7e170547fdc8ec652aafca
Eduzorkamsi <eduzorkamsi@gmail.com>
2020-09-16 01:33:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./component/home";
+import Skills from "./component/skills";
+import Projects from "./component/project";
+import Contacts from "./component/contact";
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <nav>
+            <ul className="header">
+              <li><NavLink exact to="/">Home</NavLink></li>
+              <li><NavLink to="/Skills">Skills</NavLink></li>
+              <li><NavLink to="/Projects">Projects</NavLink></li>
+              <li><NavLink to="/Contacts">Contacts</NavLink></li>
+
+            </ul>
+          </nav>
+          <div className="content">
+          <Route exact path="/" component={Home}/>
+            <Route path="/Skills" component={Skills}/>
+            <Route path="/Projects" component={Projects}/>
+            <Route path="/Contacts" component={Contacts}/>
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
