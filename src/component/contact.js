f8177fe09a3c80d803dbe5a76cc0cb1e1d75e1ce
Eduzor kamsi <40744869+Eduzorkamsi@users.noreply.github.com>
2020-09-16 02:35:45 +0100

    new file
    first commit

---
+++ src/component/contact.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { VscGithubAlt } from 'react-icons/vsc';
+import Button from './button';
+
+
+function Contacts() {
+    return (
+        <div className="Contacts">
+            <div className="Contact_header">
+                <h3>Contact Me</h3>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas cras <br></br>lectus magna sodales aenean condimentum auctor aliquet. </h4>
+
+            </div>
+            <div>
+            <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Subject</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Subject" rows="1"></textarea>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Message</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Type your message" rows="3"></textarea>
+  </div>
+</form>
+            </div>
+
+            <Button></Button>
+            <div className="footer">
+                <div className="home_icons">
+                    <a href="https://github.com/eduzorkamsi"><VscGithubAlt className="github"></VscGithubAlt></a>
+                    <a href="https://twitter.com/@kamsizoe"><i class="fab fa-twitter fa-lg"></i></a>
+                    <a href="https://www.linkedin.com/in/kamsi-eduzor-a76a2b143//"><i class="fab fa-linkedin-in fa-lg"></i></a>
+                    <a href="mailto:eduzorkamsi@gmail.com"><i class="fa fa-envelope fa-lg"></i></a>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+export default Contacts;
---
+++ src/component/project.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { VscGithubAlt } from 'react-icons/vsc';
+import image1 from "../img/Rectangle1.png";
+import image2 from "../img/Rectangle2.png";
+import image3 from "../img/Rectangle3.png";
+
+
+
+function Projects() {
+    return (
+        <div className="Project">
+            <div className="Project_header">
+                <h3>Projects</h3>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas cras <br></br>lectus magna sodales aenean condimentum auctor aliquet. </h4>
+
+            </div>
+            <div className="image_card">
+                <img className="card-img" src={image1} alt="" />
+                <div class="middle">
+                    <div class="text">
+                        <h3>Hotspots</h3>
+                        <h4>A mobile platform for finding locations.
+
+                        I built the front-end using React,
+Javascript, HTML & CSS</h4>
+                       <h3><a href="https://rippletv.netlify.app/">Check it out </a></h3> 
+                    </div>
+                </div>
+                <img className="card-img" src={image2} alt="" />
+                <div class="middle">
+                    <div class="text">
+                    <h3>Hotspots</h3>
+                        <h4>A mobile platform for finding locations.
+
+                        I built the front-end using React,
+Javascript, HTML & CSS</h4>
+                       <h3><a href="https://rippletv.netlify.app/">Check it out </a></h3> 
+                    </div>
+                </div>
+                <img className="card-img" src={image3} alt="" />
+                <div class="middle">
+                    <div class="text">
+                    <h3>Hotspots</h3>
+                        <h4>A mobile platform for finding locations.
+
+                        I built the front-end using React,
+Javascript, HTML & CSS</h4>
+                       <h3><a href="https://rippletv.netlify.app/">Check it out </a></h3> 
+
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <div className="project_icons">
+                    <a href="https://github.com/eduzorkamsi"><VscGithubAlt className="github"></VscGithubAlt></a>
+                    <a href="https://twitter.com/@kamsizoe"><i class="fab fa-twitter fa-lg"></i></a>
+                    <a href="https://www.linkedin.com/in/kamsi-eduzor-a76a2b143//"><i class="fab fa-linkedin-in fa-lg"></i></a>
+                    <a href="mailto:eduzorkamsi@gmail.com"><i class="fa fa-envelope fa-lg"></i></a>
+                </div>
+
+            </div>
+        </div>
+    );
+
+}
+export default Projects;
